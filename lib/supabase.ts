95cb3740a0620f89a0596aa5f7ada4043d13e09c
albz007 <albinjoseph229@gmail.com>
2026-06-10 23:48:39 +0530

    bug fixes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,6 @@
 // lib/supabase.ts
 // Supabase client singleton for React Native (Expo)
+import 'react-native-url-polyfill/auto';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
